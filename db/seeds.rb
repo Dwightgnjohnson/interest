5e34c62a4384dd6253761af76d5a7a58ab56b25d
dwightgnjohnson <dwightgnjohnson@gmail.com>
2014-11-09 22:39:59 -0600

    devise installed but breaking and modal styled

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
+# Interest id: 1, url: "google.com", photo: "mario.jpg", notes: "asdf", created_at: "2014-11-10 01:44:58", updated_at: "2014-11-10 01:44:58
+
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Interest.create(url: "http://www.google.com", photo: "https://fall-dev.s3.amazonaws.com/uploads/interest/photo/1/thumb_mario.jpg", notes: "blah blah blah")
